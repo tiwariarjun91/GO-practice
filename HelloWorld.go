50a2aef70d3c02fc3620f35d66034b95aa9ef0ce
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-12-18 16:59:20 +0530

    created proper workspace for running install command and listed the commands used so far

--- HelloWorld.go
+++ HelloWorld.go
@@ -11,3 +11,9 @@ func main() {
 	fmt.Println("This is your second commit")
 	fmt.Println(strings.ToUpper("Hello from me to your world, really loud"))
 }
+
+// go run filename :- Compiles and runs the .go file
+// gofmt filename:- formats the file properly and outputs on the command line
+// gofmt -w filename :- formats and saves the changes
+// go build filename :- creates executable .exe file but saves in the same directory
+
---
+++ src/HelloWorld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("Hello World !!!!")
+	fmt.Println("This is your second commit")
+	fmt.Println(strings.ToUpper("Hello from me to your world, really loud"))
+}

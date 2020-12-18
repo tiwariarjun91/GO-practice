e214d99604489d82e58c4bdee50f4693e3ef9a57
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-12-18 16:10:51 +0530

    Hello World really loud

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,8 +1,12 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
 	fmt.Println("Hello World !!!!")
 	fmt.Println("This is your second commit")
+	fmt.Println(strings.ToUpper("Hello from me to your world, really loud"))
 }

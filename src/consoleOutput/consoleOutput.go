109f6f8ba437a3969cca8fc3374026804a0db7e2
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-12-19 17:15:49 +0530

    fmt package practice with verbs and Spritnf fun

--- src/consoleOutput/consoleOutput.go
+++ src/consoleOutput/consoleOutput.go
@@ -20,7 +20,26 @@ func main(){
 		fmt.Println(stringLength)
 	}
 
+	// if you do not want to address any variable use _ in place of it's name
+
+	stringLength2, _ := fmt.Println(str1, str2, str3)
+	fmt.Println("String length 2:-", stringLength2)
+
+	// using verbs i.e %v in fmt.Printf func
+
+	aNumber := 23
+	isTrue := true
+
+	fmt.Printf("Value of aNumber is:- %v\n",aNumber)
+	fmt.Printf("Value of isTrue is:- %v\n",isTrue)
+	fmt.Printf("Value of aNumber upto 2 decimal is:- %.2f\n", float64(aNumber))
+	fmt.Printf("Data types are, %T, %T, %T, %T, %T, %T", str1, str2, str3, aNumber, isTrue, float64(aNumber))
+	stringvar := fmt.Sprintf("\n specific Data types are, %T, %T, %T, %T, %T, %T", str1, str2, str3, aNumber, isTrue, float64(aNumber))
+	fmt.Println(stringvar)
 
 }
 
 // learned about the fmt.Println function and it's return value
+// different verbs 
+// for float 0.f and float64(variablename)
+// fmt.Sprintf("")// assigns to a string variable

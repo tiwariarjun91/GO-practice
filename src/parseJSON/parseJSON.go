99dae7090a86a54342fdce250c29bf9c7ebd28cd
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-12-22 18:13:48 +0530

    Parsing JSON content

--- src/parseJSON/parseJSON.go
+++ src/parseJSON/parseJSON.go
@@ -4,14 +4,22 @@ import(
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"encoding/json"
+	"strings"
 )
+
+type Tour struct{
+	Name, Price string
+}
 
 func main(){
 	url := "http://services.explorecalifornia.org/json/tours.php"
 
 	content := contentFromServer(url)
 
-	fmt.Print(content)
+	tours := toursfromJSON(content)
+
+	fmt.Print(tours)
 
 }
 
@@ -28,4 +36,31 @@ func contentFromServer(url string) string{
 	contentString := string(contentJSON)
 
 	return contentString
+}
+
+func toursfromJSON(content string) []Tour{
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+
+	_, err := decoder.Token()
+
+	checkError(err)
+
+	var tour Tour
+	for decoder.More(){
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+	return tours
+
+
+
+}
+
+func checkError(err error){
+	if err != nil{
+		panic(err)
+	}
 }

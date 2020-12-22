c674155f1eb23decbf16889dfaf62bd74c32ecea
tiwariarjun91 <tiwariarjun91@gmail.com>
2020-12-22 15:15:38 +0530

    writing to a file

---
+++ src/writeString/writeString.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"fmt"
+	"io"
+	"os"
+)
+
+func main(){
+
+	content := "A string to be written in the file"
+
+	file, err := os.Create("./writeString.txt")
+	errorChecker(err)
+	defer file.Close()
+	ln, err := io.WriteString(file, content)
+	errorChecker(err)
+	fmt.Printf("the length is %v\n", ln)
+	fmt.Println("The Length is", ln)
+
+}
+
+
+
+	func errorChecker(err error){
+
+		if err != nil {
+			panic(err)
+		}
+	}
